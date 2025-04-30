b6935329b994fcdcba91f0a6067e8f723bac5e76
Stalin francis <educaysoft@gmail.com>
2025-04-29 21:54:30 -0500

    mejoras

--- app/controllers/SexoController.php
+++ app/controllers/SexoController.php
@@ -92,7 +92,7 @@ public function update() {
         echo "Formulario recibido";  // Verificar si llega el formulario
         if (isset($_POST['nombre'])) {
             $this->sexo->nombre = $_POST['nombre'];
-            $this->sexo->id = $_POST['id'];
+            $this->sexo->idsexo = $_POST['id'];
             if ($this->sexo->update()) {
                 echo "Sexo actualizado exitosamente";
                 // Redirigir o mostrar un mensaje de éxito
