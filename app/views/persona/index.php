11faaf4b9e963be3ee635a95245171582c50efcc
Dayana Estupiñan <Anthony.tenorio.espinoza@utelvt.edu.ec>
2025-05-12 16:14:35 -0500

    cambio de titulo

--- app/views/persona/index.php
+++ app/views/persona/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listar Personas</title>
+    <title>Lista de persona Personas</title>
     <link rel="stylesheet" href="/apple5a/public/css/style.css">
 </head>
 <body>
